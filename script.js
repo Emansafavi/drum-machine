203221fbf0faba5ddd421fd0b8671cb57f8dc633
Emansafavi <emansafavibayat@gmail.com>
2026-08-31 03:12:53 +0200

    js complete basic function and html buttons added instead of div + css smoothness

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const pads = document.querySelectorAll(".drum-pad")
+const display = document.getElementById("display");
+  function triggerPad(pad) {
+
+    const audio = pad.querySelector(".clip")
+        audio.currentTime = 0;
+        audio.play()
+      display.innerText = pad.id;
+  }
+pads.forEach(pad =>
+{
+  pad.addEventListener("click", () =>{
+    console.log('You clicked me!')
+    triggerPad(pad)
+    pad.classList.add("drum-pad-click")
+    setTimeout(() => {
+      pad.classList.remove("drum-pad-click")},60)
+  
+    
+  })
+  })
+  document.addEventListener("keydown", (event) =>{
+ 
+    const keyboard = event.key.toLowerCase()
+    const what = document.getElementById(`${keyboard.toUpperCase()}`)
+    console.log(`you clicked ${keyboard}`)
+    if (what===null) return;
+    triggerPad(what.parentElement)
+    what.parentElement.classList.add("drum-pad-click")
+    setTimeout(() => {
+     what.parentElement.classList.remove("drum-pad-click")},60)
+   
+});
