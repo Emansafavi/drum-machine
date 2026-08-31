d3bd76cee7f350b8a97a1896928615e3c400a57e
Emansafavi <emansafavibayat@gmail.com>
2026-08-31 20:51:31 +0200

    add local assets instead of cdn file. removing the id of each button and the name now is coming from src name.

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-
-const pads = document.querySelectorAll(".drum-pad")
+const pads = document.querySelectorAll(".drum-pad");
 const display = document.getElementById("display");
-  function triggerPad(pad) {
 
-    const audio = pad.querySelector(".clip")
-        audio.currentTime = 0;
-        audio.play()
-      display.innerText = pad.id;
-  }
-pads.forEach(pad =>
-{
-  pad.addEventListener("click", () =>{
-    console.log('You clicked me!')
-    triggerPad(pad)
-    pad.classList.add("drum-pad-click")
+function triggerPad(pad) {
+  const audio = pad.querySelector(".clip");
+  audio.currentTime = 0;
+  audio.play();
+  display.innerText = pad.id;
+}
+pads.forEach((pad) => {
+  const audio = pad.querySelector(".clip");
+
+  // picking the file name to display without the extention via a little regex showoff :)
+  const fileName = decodeURIComponent(audio.src.split("/").pop())
+  pad.id = fileName.replace(/\.[^/.]+$/,"")
+  pad.addEventListener("click", () => {
+    console.log("You clicked me!");
+    triggerPad(pad);
+    pad.classList.add("drum-pad-click");
     setTimeout(() => {
-      pad.classList.remove("drum-pad-click")},60)
-  
-    
-  })
-  })
-  document.addEventListener("keydown", (event) =>{
- 
-    const keyboard = event.key.toLowerCase()
-    const what = document.getElementById(`${keyboard.toUpperCase()}`)
-    console.log(`you clicked ${keyboard}`)
-    if (what===null) return;
-    triggerPad(what.parentElement)
-    what.parentElement.classList.add("drum-pad-click")
-    setTimeout(() => {
-     what.parentElement.classList.remove("drum-pad-click")},60)
-   
+      pad.classList.remove("drum-pad-click");
+    }, 60);
+  });
+});
+document.addEventListener("keydown", (event) => {
+  const keyboard = event.key.toLowerCase();
+  const what = document.getElementById(`${keyboard.toUpperCase()}`);
+  console.log(`you clicked ${keyboard}`);
+  if (what === null) return;
+  triggerPad(what.parentElement);
+  what.parentElement.classList.add("drum-pad-click");
+  setTimeout(() => {
+    what.parentElement.classList.remove("drum-pad-click");
+  }, 60);
 });
